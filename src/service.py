9b5891fb5efc0cdfe39eab6d9d7798c4638c0337
Thiago Martins <thiago.fmartins@outlook.com>
2024-09-21 08:20:40 -0300

    add get_conversation and list_conversations to rpc service

--- src/service.py
+++ src/service.py
@@ -1,5 +1,6 @@
 from nameko.rpc import rpc
 from .chatbot import ChatbotService
+from . import orm
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,6 +8,20 @@ load_dotenv()
 
 class RPCChatbot:
     name = "chatbot"
+
+    @rpc
+    def list_conversations(self, user_id: int) -> list[dict]:
+        return orm.conversations.list(
+            filter={"user_id": user_id, "archived": False},
+            mode="all",
+            order={"updated_at": "desc"},
+        )
+
+    @rpc
+    def get_conversation(self, user_id: int, conversation_id: int) -> dict:
+        return orm.conversations.get(
+            by=["id", "user_id", "archived"], value=[conversation_id, user_id, False]
+        )
 
     @rpc
     def send_message(
